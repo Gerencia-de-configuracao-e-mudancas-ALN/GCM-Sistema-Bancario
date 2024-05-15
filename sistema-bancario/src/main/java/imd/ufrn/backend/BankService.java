a6c55ae4cf79633544c2a23f85be80a0ad59e6ec
leodavidbr <rbdivadoel@gmail.com>
2024-05-15 00:37:46 -0300

    bonus de tranferencia alterado para 1 ponto para cada 150 reais recebidos

--- sistema-bancario/src/main/java/imd/ufrn/backend/BankService.java
+++ sistema-bancario/src/main/java/imd/ufrn/backend/BankService.java
@@ -58,7 +58,7 @@ public class BankService {
 
         if (destinationAccount instanceof BonusAccount) {
             int punctuation = ((BonusAccount) destinationAccount).getPunctuation();
-            ((BonusAccount) destinationAccount).setPunctuation(punctuation + (int) (value / 200));
+            ((BonusAccount) destinationAccount).setPunctuation(punctuation + (int) (value / 150));
         }
         bankRepository.saveAccount(destinationAccount);
         return true;
