86734ccb4c50ecaa9e3f81d8098544617adb2b48
neylanepl <neylanepl@gmail.com>
2024-07-21 17:00:31 -0300

    tests: testes unitários para consultar saldo, crédito, débito, transferência e render juros

--- sistema-bancario-backend/src/test/java/imd/ufrn/service/BankServiceTests.java
+++ sistema-bancario-backend/src/test/java/imd/ufrn/service/BankServiceTests.java
@@ -2,9 +2,13 @@ package imd.ufrn.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -69,7 +73,7 @@ public class BankServiceTests {
     }
 
     @Nested
-    public class getAccountByNumber {
+    public class GetAccountByNumber {
         private int accountNumber = 10;
         private double balance = 200;
 
@@ -107,4 +111,203 @@ public class BankServiceTests {
         }
     }
 
+    @Nested
+    public class CheckBalance {
+        private int accountNumber = 10;
+        private double balance = 200;
+
+        @Test
+        public void shouldCheckBalance() {
+            var expectedAccount = new Account(accountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.checkBalance(accountNumber);
+
+            assertEquals(balance, response);
+        }
+    }
+
+    @Nested
+    public class RealizeCredit {
+        private int accountNumber = 10;
+        private double balance = 200;
+
+        @Test
+        public void shouldRealizeCredit() {
+            double value = 100;
+            var expectedAccount = new Account(accountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.realizeCredit(accountNumber, value, false);
+
+            assertEquals(balance + value, response);
+        }
+
+        @Test
+        public void shouldNotRealizeCreditWhenValueIsNegative() {
+            double value = -100;
+            var expectedAccount = new Account(accountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.realizeCredit(accountNumber, value, false);
+
+            assertEquals(balance, response);
+        }
+
+        @Test
+        public void shouldAddBonusPointsForBonusAccount() {
+            double value = 100;
+            var expectedAccount = new BonusAccount(accountNumber, balance, 10);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.realizeCredit(accountNumber, value, false);
+
+            assertEquals(balance + value, response);
+            assertEquals(11, expectedAccount.getPunctuation());
+        }
+    }
+
+    @Nested
+    public class RealizeDebit {
+        private int accountNumber = 10;
+        private double balance = 200;
+
+        @Test
+        public void shouldRealizeDebit() {
+            double value = 100;
+            var expectedAccount = new Account(accountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.realizeDebit(accountNumber, value);
+
+            assertTrue(response.isPresent());
+            assertEquals(balance - value, response.get());
+        }
+
+        @Test
+        public void shouldNotRealizeDebitWhenValueIsNegative() {
+            double value = -100;
+            var expectedAccount = new Account(accountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.realizeDebit(accountNumber, value);
+
+            assertTrue(response.isEmpty());
+            verify(bankRepository, times(0)).saveAccount(expectedAccount);
+        }
+
+        @Test
+        public void shouldNotAllowNegativeBalance() {
+            double value = 300;
+            var expectedAccount = new Account(accountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(accountNumber)).thenReturn(expectedAccount);
+
+            var response = bankService.realizeDebit(accountNumber, value);
+
+            assertTrue(response.isEmpty());
+            verify(bankRepository, times(0)).saveAccount(expectedAccount);
+
+        }
+    }
+
+    @Nested
+    public class RealizeTransfer {
+        private int originAccountNumber = 10;
+        private int destinationAccountNumber = 20;
+        private double balance = 200;
+
+        @Test
+        public void shouldRealizeTransfer() {
+            double value = 100;
+            var originAccount = new Account(originAccountNumber, balance);
+            var destinationAccount = new Account(destinationAccountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(originAccountNumber)).thenReturn(originAccount);
+            Mockito.when(bankRepository.getAccountByAccountNumber(destinationAccountNumber)).thenReturn(destinationAccount);
+
+            var response = bankService.realizeTransfer(originAccountNumber, destinationAccountNumber, value);
+
+            assertTrue(response);
+            assertEquals(balance - value, originAccount.getBalance());
+            assertEquals(balance + value, destinationAccount.getBalance());
+        }
+
+        @Test
+        public void shouldNotAllowNegativeTransferValue() {
+            double value = -100;
+            var originAccount = new Account(originAccountNumber, balance);
+            var destinationAccount = new Account(destinationAccountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(originAccountNumber)).thenReturn(originAccount);
+            Mockito.when(bankRepository.getAccountByAccountNumber(destinationAccountNumber)).thenReturn(destinationAccount);
+
+            var response = bankService.realizeTransfer(originAccountNumber, destinationAccountNumber, value);
+
+            assertFalse(response);
+            assertEquals(balance, originAccount.getBalance());
+            assertEquals(balance, destinationAccount.getBalance());
+        }
+
+        @Test
+        public void shouldNotAllowNegativeBalanceAfterTransfer() {
+            double value = 300;
+            var originAccount = new Account(originAccountNumber, balance);
+            var destinationAccount = new Account(destinationAccountNumber, balance);
+            Mockito.when(bankRepository.getAccountByAccountNumber(originAccountNumber)).thenReturn(originAccount);
+            Mockito.when(bankRepository.getAccountByAccountNumber(destinationAccountNumber)).thenReturn(destinationAccount);
+
+            var response = bankService.realizeTransfer(originAccountNumber, destinationAccountNumber, value);
+
+            assertFalse(response);
+            assertEquals(balance, originAccount.getBalance());
+            assertEquals(balance, destinationAccount.getBalance());
+        }
+
+        @Test
+        public void shouldAddBonusPointsForBonusAccount() {
+            double value = 200;
+            var originAccount = new Account(originAccountNumber, balance);
+            var destinationAccount = new BonusAccount(destinationAccountNumber, balance, 10);
+            Mockito.when(bankRepository.getAccountByAccountNumber(originAccountNumber)).thenReturn(originAccount);
+            Mockito.when(bankRepository.getAccountByAccountNumber(destinationAccountNumber)).thenReturn(destinationAccount);
+
+            var response = bankService.realizeTransfer(originAccountNumber, destinationAccountNumber, value);
+
+            assertTrue(response);
+            assertEquals(balance - value, originAccount.getBalance());
+            assertEquals(balance + value, destinationAccount.getBalance());
+            assertEquals(11, ((BonusAccount) destinationAccount).getPunctuation());
+        }
+    }
+
+    @Nested
+    public class PayFees {
+        @Test
+        public void shouldRenderInterestForSavingsAccount() {
+            double fee = 5.0;
+
+            var savingsAccount1 = new SavingsAccount(10, 200.0);
+            var savingsAccount2 = new SavingsAccount(20, 300.0);
+            var savingsAccount3 = new SavingsAccount(30, 400.0);
+
+            Map<Integer, Account> accountsMap = new HashMap<>();
+            accountsMap.put(savingsAccount1.getAccountNumber(), savingsAccount1);
+            accountsMap.put(savingsAccount2.getAccountNumber(), savingsAccount2);
+            accountsMap.put(savingsAccount3.getAccountNumber(), savingsAccount3);
+
+            Mockito.when(bankRepository.findAll()).thenReturn(accountsMap);
+
+            double expectedBalance1 = savingsAccount1.getBalance() + (savingsAccount1.getBalance() * fee / 100);
+            double expectedBalance2 = savingsAccount2.getBalance() + (savingsAccount2.getBalance() * fee / 100);
+            double expectedBalance3 = savingsAccount3.getBalance() + (savingsAccount3.getBalance() * fee / 100);
+
+            bankService.payFees(fee);
+
+            assertEquals(expectedBalance1, savingsAccount1.getBalance(), 0.01);
+            assertEquals(expectedBalance2, savingsAccount2.getBalance(), 0.01);
+            assertEquals(expectedBalance3, savingsAccount3.getBalance(), 0.01);
+
+            verify(bankRepository, times(1)).saveAccount(savingsAccount1);
+            verify(bankRepository, times(1)).saveAccount(savingsAccount2);
+            verify(bankRepository, times(1)).saveAccount(savingsAccount3);
+        }
+    }
 }
